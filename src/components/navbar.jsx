4ede586adce277a30d3f6a098080d50de20b0032
Th-Tomas <thtomas@yahoo.com>
2023-07-31 13:13:35 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./navbar.css";
+
+ const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
